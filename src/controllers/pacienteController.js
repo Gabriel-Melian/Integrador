91acf5e505097faa894929d7152f51c92e2ef028
Gabriel Melian <125325749+Gabriel-Melian@users.noreply.github.com>
2024-06-05 21:30:59 -0300

    Se logra interactuar con BD

--- src/controllers/pacienteController.js
+++ src/controllers/pacienteController.js
@@ -1,7 +1,7 @@
 import Paciente from "../models/paciente.js";
 import { conn } from "../models/db.js";
 
-export async function getPacientes() {//Consulta para seleccionar todos los pacientes.
+export async function getPacientes() {//Mostrar todos los pacientes
     const query = "SELECT * FROM paciente";
     try {
         const result = await conn.execute(query);
@@ -15,3 +15,74 @@ export async function getPacientes() {//Consulta para seleccionar todos los paci
     }
 }
 //Consultar,guardar, buscar paciente por id,nombre,dni.
+
+export async function guardarPaciente(paciente) {//Insertar paciente. Desestructuracion.
+    //Falta validacion.
+    const query = "INSERT INTO paciente (nombre, apellido, dni, fechaNac, email, sexo, idObraSocial) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    if (paciente === null) return console.log("Error: Paciente es null");
+    const values = [paciente.nombre, paciente.apellido, paciente.dni, paciente.fechaNac, paciente.email, paciente.sexo, paciente.idObraSocial];
+    try {
+        const result = await conn.execute(query, values);
+        paciente.id = result[0].insertId;
+        //console.log(paciente.id);
+        const pacienteGuardado = await getPaciente(paciente.id);
+        return pacienteGuardado;
+    } catch (error) {
+        console.error("Error executing query:", error);
+        throw error;
+    }
+}
+
+export async function getPaciente(id) {//Buscar por id
+    const query = "SELECT * FROM paciente WHERE id = ?";
+    try {
+        const result = await conn.execute(query, [id]);
+        if(result.length === 0) return console.log('El ID paciente ' + id + ' no existe');
+        console.log(result);
+        const [rows] = result;
+        return rows[0];
+    } catch (error) {
+        console.error("Error executing query:", error);
+        throw error;
+    }
+}
+
+export async function getPacienteDni(dni) {//Buscar por DNI
+    const query = "SELECT * FROM paciente WHERE dni = ?";
+    try {
+        const result = await conn.execute(query, [dni]);
+        if(result.length === 0) return console.log('El DNI paciente ' + dni + ' no existe');
+        console.log(result);
+        const [rows] = result;
+        return rows[0];
+    } catch (error) {
+        console.error("Error executing query:", error);
+        throw error;
+    }
+}
+
+export async function getPacienteNombre(paciente) {//Buscar por nombre
+    const query = "SELECT * FROM paciente WHERE nombre = ?";
+    try {
+        const result = await conn.execute(query, [paciente]);
+        if(result.length === 0) return console.log('El Paciente ' + paciente + ' no existe');
+        console.log(result);
+        const [rows] = result;
+        return rows[0];
+    } catch (error) {
+        console.error("Error executing query:", error);
+        throw error;
+    }
+}
+
+export async function desactivarPaciente(id) {//Borrar paciente
+    const query = "UPDATE paciente SET estado = 0 WHERE id = ?";
+    try {
+        const result = await conn.execute(query, [id]);
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error("Error executing query:", error);
+        throw error;
+    }
+}
--- src/router/pacienteRouter.js
+++ src/router/pacienteRouter.js
@@ -1,14 +1,29 @@
-import { getPacientes } from "../controllers/pacienteController.js";
+import { getPacientes, getPaciente, desactivarPaciente, getPacienteDni, guardarPaciente } from "../controllers/pacienteController.js";//Importar cada metodo
 import { Router } from "express";
+
 const router = Router();
-router.get("/pacientes", async (req, res) => {
+
+router.get("/pacientes", async (req, res) => {//http://localhost:3000/pacientes (GET Thunderclient)
     const pacientes = await getPacientes();
     console.log(pacientes);
     res.status(200).json(pacientes);
 });
 
+router.post("/pacientes/nuevo", async (req, res) => {//http://localhost:3000/nuevo (POST Thunderclient)
+    const paciente = req.body;
+    const result = await guardarPaciente(paciente);
+    res.status(200).json(result);
+});
+
+router.get("/pacientes/:id", async (req, res) => {//http://localhost:3000/pacientes/2 (GET Thunderclient)
+    const id = req.params.id;//params mapea los parametros que vienen en la url
+    const paciente = await getPaciente(id);
+    res.status(200).json(paciente);
+});
+
 export default router;
-//router.get("/pacientes", getPacientes);
-//router.post("/pacientes", savePaciente);
+
+
+//router.delete("/pacientes/:id", desactivarPaciente);
 //router.put("/pacientes/:id", updatePaciente);
 //router.patch("/pacientes/:id", updatePaciente);//Actualiza 1 solo atributo que vos le envies.
